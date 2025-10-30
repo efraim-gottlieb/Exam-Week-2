d885a04ac8d08818ee975c0492eef55fea68cd2f
EfraimGottlieb <efgo583208979@gmail.com>
2025-10-30 10:44:30 +0200

    add hand calc func

--- core/game_logic.py
+++ core/game_logic.py
@@ -1,9 +1,17 @@
 def calculate_hand_value(hand: list[dict]) -> int:
-    return int
+    total = 0
+    for card in hand:
+        if card['rank'] == 'A':
+            total += 1
+        elif card['rank'] in ['J', 'Q', 'K']:
+            total += 10
+        else:
+            total += int(card['rank'])
+    return total
+    
 
 def deal_two_each(deck: list[dict], player: dict, dealer: dict) -> None:
     pass
 
 def dealer_play(deck: list[dict], dealer: dict) -> bool:
     return bool
-
