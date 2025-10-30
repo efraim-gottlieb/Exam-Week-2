8649902799a8b2c5ffe64051d6d6818e5be14f52
EfraimGottlieb <efgo583208979@gmail.com>
2025-10-30 10:45:37 +0200

    fix suit name

--- core/deck.py
+++ core/deck.py
@@ -5,9 +5,9 @@ def build_standard_deck() -> list[dict]:
     deck = []
     for suit in ["H", "C", "D", "S"]:
         for special_rank in ['J', 'Q', 'K', 'A']:
-            deck.append({'rank' : special_rank, 'suite' : suit})
+            deck.append({'rank' : special_rank, 'suit' : suit})
         for i in range(2,11):
-            deck.append({'rank' : str(i +1), 'suite' : suit})
+            deck.append({'rank' : str(i +1), 'suit' : suit})
     return deck
 
 def shuffle_by_suit(deck: list[dict], swaps: int = 5000) -> list[dict]:
@@ -15,10 +15,10 @@ def shuffle_by_suit(deck: list[dict], swaps: int = 5000) -> list[dict]:
         condition_met = False
         while not condition_met:
             i = randint(0, len(deck) -1)
-            suite = deck[i]['suite']
+            suit = deck[i]['suit']
             j = randint(0, len(deck) -1)
             not_equal = i != j
-            suite_condition_met = not j % 5 if suite == 'H' else not j % 3 if suite == 'C' else not j % 2 if suite == 'D' else not j % 7 if suite == 'S' else ''
-            condition_met = not_equal and suite_condition_met
+            suit_condition_met = not j % 5 if suit == 'H' else not j % 3 if suit == 'C' else not j % 2 if suit == 'D' else not j % 7 if suit == 'S' else ''
+            condition_met = not_equal and suit_condition_met
         deck[i], deck[j] = deck[j], deck[i]
     return deck
