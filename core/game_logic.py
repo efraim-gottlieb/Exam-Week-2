9177108865a798c8b5128c0ca75280dee159fa1e
EfraimGottlieb <efgo583208979@gmail.com>
2025-10-30 11:01:33 +0200

    add dealer_play func

--- core/game_logic.py
+++ core/game_logic.py
@@ -19,5 +19,11 @@ def deal_two_each(deck: list[dict], player: dict, dealer: dict) -> None:
     print(f'player hand value :{calculate_hand_value(player['hand'])}')
 
 def dealer_play(deck: list[dict], dealer: dict) -> bool:
-    return bool
-
+    while calculate_hand_value(dealer['hand']) <= 17:
+        dealer['hand'].append(deck.pop())
+    if calculate_hand_value(dealer['hand']) > 21 :
+        print('dialer loss !')
+        return False
+    else:
+        return True
+    
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from core.deck import build_standard_deck
 from core.deck import shuffle_by_suit
 # from core.game_logic import run_full_game
-from core.game_logic import deal_two_each
+from core.game_logic import dealer_play
 
 if __name__ == '__main__':
     deck = build_standard_deck()
     shuffle_by_suit(deck)
-    print(deck)
     player = {"hand": []} 
     dealer = {"hand": []}
-    deal_two_each(deck, player, dealer)
+    dealer_play(deck, dealer)
+
+
     # run_full_game(deck, player, dealer)
