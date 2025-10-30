0185030ce4583534b901d9025ac6d7a67242031f
EfraimGottlieb <efgo583208979@gmail.com>
2025-10-30 09:46:15 +0200

    add deck func

--- core/deck.py
+++ core/deck.py
@@ -1,5 +1,17 @@
 def build_standard_deck() -> list[dict]:
-    return [{}]
+    deck = []
+    for suit in ["H", "C", "D", "S"]:
+        for special_rank in ['J', 'Q', 'K', 'A']:
+            deck.append({'rank' : special_rank, 'suite' : suit})
+        for i in range(2,11):
+            deck.append({'rank' : str(i +1), 'suite' : suit})
+    return deck
+
+a = build_standard_deck()
+for i in a:
+    print(i)
+print(len(a))
+
 
 def shuffle_by_suit(deck: list[dict], swaps: int = 5000) -> list[dict]:
     return [{}]
